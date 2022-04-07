415e3ccdb74f7406ae280eba69fa9f5ac79fb01a
Lucas <lucasalonso11800@gmail.com>
2022-04-07 17:59:21 -0300

    Footer and header components

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <p class="quote">“Time, wondrous time, gave me the blues and then purple-pink skies.”</p>
+    <p>Taylor Swift</p>
+  </footer>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FooterComponent",
+});
+</script>
+
+<style scoped>
+footer {
+  background-color: #c51162;
+  padding: 8px;
+}
+p {
+    text-align: center;
+    line-height: 24px;
+    color: #FFF;
+}
+.quote {
+  font-style: italic;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <nav>
+      <button>
+        <router-link to="/">Taylor Swift Survey</router-link>
+      </button>
+      <button>
+        <router-link to="/results">See results</router-link>
+      </button>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HeaderComponent",
+});
+</script>
+
+<style>
+header {
+  background-color: #c51162;
+}
+nav {
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+}
+button {
+  padding: 20px;
+  border: none;
+  background: transparent;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+}
+button:hover {
+      background-color: #950142;
+}
+a {
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 16px;
+  color: #fff;
+  font-weight: 500;
+  line-height: 1.75;
+}
+</style>
